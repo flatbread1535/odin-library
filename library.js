95bb09f8a8e47fcbe5dbd9ddadb112a8785b406e
Adam <ajlarson0731@gmail.com>
2026-05-19 02:17:23 -0400

    Added a createBook function that gets the data from the form and then adds that book to the library. Also added event listeners for the new book button, exit form button, and submit button.

--- library.js
+++ library.js
@@ -1,7 +1,33 @@
 const myLibrary = [];
 
+const form = document.querySelector("form");
+const newBookButton = document.querySelector(".new-book-btn");
+const dialog = document.querySelector(".book-dialog");
+
+/* Listener for new book button to display modal */
+newBookButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+/* Listener for exit button to exit the modal */
+const exitButton = document.querySelector(".form-exit-btn");
+exitButton.addEventListener("click", () => {
+    event.preventDefault();
+    form.reset();
+    dialog.close();
+});
+
+/* Listener for submit button to submit the form */
+const submit = document.querySelector("button[type=\"submit\"]");
+submit.addEventListener("click", () => {
+    event.preventDefault();
+    createBook();
+    form.reset();
+    dialog.close();
+});
+
+/* Book constructor */
 function Book(title, author, pages, hasRead) {
-    // the constructor...
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -9,6 +35,7 @@ function Book(title, author, pages, hasRead) {
     this.id = crypto.randomUUID();
 }
 
+/* Constructs a new book object and adds the book to the library */
 function addBookToLibrary(title, author, pages, hasRead) {
     // take params, create a book then store it in the array
     const book = new Book(title, author, pages, hasRead);
@@ -19,27 +46,18 @@ function displayBooks() {
     // loops through the array and displays each book on the page
     const container = document.querySelector(".container");
     for (let i = 0; i < myLibrary.length; i++) {
+        const bookObject = myLibrary[i];
         const book = document.createElement("div");
-        // styling the book itself, i.e., it's "structure"
-        book.classList.add("book");
-        // TODO: Create more elements and styling and append to book
-        // TODO: Modify text content for new elements to append to book reflecting book info
         container.appendChild(book);
     }
 }
 
-const newBookButton = document.querySelector(".new-book-btn");
-const dialog = document.querySelector(".book-dialog");
-newBookButton.addEventListener("click", () => {
-    dialog.showModal();
-});
-
-const exitButton = document.querySelector(".form-exit-btn");
-exitButton.addEventListener("click", () => {
-    event.preventDefault();
-    dialog.close();
-});
-
+/* Gets data from form  to create a book */
 function createBook() {
-    
+    const title = form.elements["title"].value;
+    const author = form.elements["author"].value;
+    const pages = Number(form.elements["pages"].value);
+    const hasRead = form.elements["has-read"].value;
+
+    addBookToLibrary(title, author, pages, hasRead);   
 }
