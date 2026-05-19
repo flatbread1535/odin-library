3efe752f7412ab62c63d1df7553c75b92ec74309
Adam <ajlarson0731@gmail.com>
2026-05-19 18:06:42 -0400

    Fixed styling issue with input field for pages. Moved dialog outside of container in HTML document so it is not cleared. Implemented a function to update information in sidebar when adding books. Started more implementation of displayBooks function.

--- library.js
+++ library.js
@@ -40,15 +40,58 @@ function addBookToLibrary(title, author, pages, hasRead) {
     // take params, create a book then store it in the array
     const book = new Book(title, author, pages, hasRead);
     myLibrary.push(book);
+    addBookInfo(book);
+    displayBooks();
+}
+
+/* Updates sidebar information when a book is created */
+function addBookInfo(book) {
+    // Adds to total book number
+    const bookNumInfo = document.querySelector(".book-num .info-val");
+    let bookNum = Number(bookNumInfo.textContent);
+    bookNum++;
+    bookNumInfo.textContent = bookNum;
+
+    // Adds to complete or incomplete books number
+    const readValue = document.querySelector('input[name="has-read"]:checked').value;
+    if (readValue === "read") {
+        const completeInfo = document.querySelector(".complete-books .info-val");
+        let completeNum = Number(completeInfo.textContent);
+        completeNum++;
+        completeInfo.textContent = completeNum;
+    } else {
+        const incompleteInfo = document.querySelector(".incomplete-books .info-val");
+        let incompleteNum = Number(incompleteInfo.textContent);
+        incompleteNum++;
+        incompleteInfo.textContent = incompleteNum;
+    }
+
+    // Adds to the total page number
+    const pageNumInfo = document.querySelector(".total-pages .info-val");
+    let pageNum = Number(pageNumInfo.textContent);
+    pageNum += book.pages;
+    pageNumInfo.textContent = pageNum;
 }
 
 function displayBooks() {
     // loops through the array and displays each book on the page
     const container = document.querySelector(".container");
+    container.replaceChildren();
     for (let i = 0; i < myLibrary.length; i++) {
+        // Get book object itself for data
         const bookObject = myLibrary[i];
-        const book = document.createElement("div");
-        container.appendChild(book);
+        
+        // Grab the book card, add the class for styling, and append
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        container.appendChild(bookCard);
+
+        const bookCardTop = document.createElement("div");
+        const bookCardBottom = document.createElement("div");
+        bookCardTop.classList.add("book-card-top");
+        bookCardBottom.classList.add("book-card-bottom");
+        bookCard.appendChild(bookCardTop);
+        bookCard.appendChild(bookCardBottom);
     }
 }
 
